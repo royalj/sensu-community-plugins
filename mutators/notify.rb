810d8919411b4bcfef7c4753427940b185d76d75
Jacob Royal <jacob.royal@buckle.com>
2015-03-05 14:12:21 -0600

    cleanup

--- mutators/notify.rb
+++ mutators/notify.rb
@@ -4,16 +4,49 @@
 # ===
 #
 # DESCRIPTION:
-#   Check against custom parameters within the check to determine if the
-#   handler should alert. Assumes 7 days a week.
+#   This mutator allows a custom notification period to be passed in via a custom check
+#   parameter(notify_period). The notify_period parameter must be an array that contains exactly
+#   two string values. The first value will always be the start time for the notification period.
+#   The second parameter will always be the end time. The start/end times should be formatted in
+#   24 hour clock format with hours and minutes seperated by a comma. If you are defining an
+#   overnight notification period you must also set the custom check parameter
+#   notify_overnight => true.
 #
 # OUTPUT:
 #   mutated JSON event
 #
-# USAGE:
-#   TODO
+# USAGE: (Buckle specific)
+#   Within puppet check definitions:
+#     sensu::check { 'example':
+#       ensure  => true,
+#       command => '...',
+#       ...
+#       custom  => {
+#         ...
+#         notify_period => ['8:00', '17:00']
+#       }
 #
-#PLATFORM:
+#     sensu::check { 'example2':
+#       ensure  => true,
+#       command => '...',
+#       ...
+#       custom  => {
+#         ...
+#         notify_period => ['19:00', '7:00'],
+#         notify_overnight => true
+#       }
+#   Within puppet handler defintion:
+#     sensu::handler { 'default':
+#       type      => 'set',
+#       command   => true,
+#       handlers  => $default_handler_array,
+#       mutator   => 'notify'
+#       config    => {
+#         dashboard_link => $dashboard_link,
+#       }
+#     }
+#
+# PLATFORM:
 #   all
 #
 # DEPENDENCIES:
@@ -24,54 +57,27 @@
 #   Released under the same terms as Sensu (the MIT license); see LICENSE
 #   for details.
 
-require 'rubygems' if RUBY_VERSION < '1.9.0'
-require 'json'
-require 'time'
-
-# GOAL/REQUIREMENTS
-# Pass in an array via the check that contains 2 elements
-#   1st element is ALWAYS the start time
-#   2nd element is ALWAYS the end time
-# Also check can be set to notify overnight with notify_overnight flag == true in custom check param
-# Start/end times in 24 hour clock format
-# If array does not exist, notify = true, meaning check
-# will notify 24x7
-# If array exists and time.now.between start/end time then notify = true (add validation for time format)
-# Special logic to handle overnight periods
-# merge element into event => notify: <true/false>
-#
-# Within default handler just need to utilize the notify_period and notify_overnight(optional) variable
-# e.g. unless notify == false then alert
 # Example usage:
-#   sensu::check { 'example':
-#     ensure  => true,
-#     command => '...',
-#     ...
-#     custom  => {
-#       ...
-#       notify_period => ['8:00', '17:00']
-#     }
-#
-#   sensu::check { 'example2':
-#     ensure  => true,
-#     command => '...',
-#     ...
-#     custom  => {
-#       ...
-#       notify_period => ['19:00', '7:00'],
-#       notify_overnight => true
-#     }
+
 # For each handler, we would then add a couple lines of logic to check if the event should alert
 # e.g.  unless @event['check']['notify']
+#         puts 'Event not within notification period'
 #         exit(0)
 #       end
 #
 # NOTES:
 # What happens if Sensu server goes down messages in queue that need to alert don't hit the notification period?
 
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'json'
+require 'time'
+
+##### MAIN #####
+
+validate_period
 
 event       = JSON.parse(STDIN.read, symbolize_names: true)
-@t          = Time.now
+@time       = Time.now
 @notify     = true
 
 # If notify_period is not set then assume 24x7 notification period and exit
@@ -86,40 +92,6 @@ end
 @overnight    = event[:check][:notify_overnight] || false
 reg_validate  = /\d{1,2}:\d{2}/
 
-# Check to see if the event timestamp is within the notification period given
-def notification_period? 
-  if @t.between?(Time.parse("#{@start_time}"), Time.parse("#{@end_time}"))
-    @notify
-  else
-    @notify == false
-  end
-  # An overnight notification period (where @end_time < @start_time, e.g. 18:00 - 6:00) will
-  # throw an error so we need to catch the error and check if we want to notify overnight or not.
-  rescue ArgumentError
-    overnight?  
-end
-
-
-def overnight?
-  if @overnight && @end_time <= @t && @t >= @start_time
-    true
-  else
-    raise ArgumentError
-      puts "[ERROR] Start time is after End time, overnight periods must have notify_overnight flag set"
-      exit(2)
-  end
-end
-
-def validate_period
-  unless @start_time == reg_validate || @end_time == reg_validate
-    raise ArgumentError
-      puts "[ERROR] notify period times need to be formatted in <hour>:<minute>"
-      exit(2)
-  end
-end
-
-validate_period
-
 # mutate the event based on whether it should alert or not
 if notification_period?
   event.merge!(notify: true)
@@ -130,3 +102,31 @@ end
 # output modified event
 puts event.to_json
 exit(0)
+
+# Check to see if the event timestamp is within the notification period given
+def notification_period? 
+  if @time.between?(Time.parse("#{@start_time}"), Time.parse("#{@end_time}"))
+    @notify
+  else
+    !@notify
+  end
+  # An overnight notification period (where @end_time < @start_time, e.g. 18:00 - 6:00) will
+  # throw an error so we need to catch the error and check if we want to notify overnight or not.
+  rescue ArgumentError
+    overnight?  
+end
+
+
+def overnight?
+  if @overnight && @end_time <= @time && @time >= @start_time
+    true
+  else
+    raise ArgumentError
+  end
+end
+
+def validate_period
+  unless @start_time == reg_validate || @end_time == reg_validate
+    raise ArgumentError.new('Invalid start and/or end time')
+  end
+end
